6159240b6336fcd9580db4ac1ba859b55561cfba
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2022-12-22 00:39:23 -0300

    add integration test

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,31 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+
+import { app } from "../../../../app";
+import createConnection from "../../../../database";
+
+let connection: Connection;
+
+describe("Create user controller", () => {
+	beforeAll(async () => {
+		connection = await createConnection();
+    await connection.runMigrations();
+	});
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "Jonathan",
+        email: "jontvrs@gmail.com",
+        password: "123",
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
